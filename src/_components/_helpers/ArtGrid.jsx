bafbe17fb3257d2c499314c887a12dec24b749bf
El Giorgio <jruanova1987@gmail.com>
2022-05-17 13:25:57 -0300

    updates

--- src/_components/_helpers/ArtGrid.jsx
+++ src/_components/_helpers/ArtGrid.jsx
@@ -4,49 +4,53 @@ import { isMobile } from "react-device-detect";
 
 import { GalleryArray } from "../DataArray";
 
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
 
 // ------------------------------------------------------------
 
-function Item({ galleryartimg }) {
-  const ImgContainer = styled.div`
-    transition: 1s;
-    background-image: url(${galleryartimg});
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-    border-color: none;
-    width: 100%;
-    height: ${isMobile ? "10rem" : "15rem"};
-    border-radius: 5px;
-  `;
-
-  return (
-    <>
-      {galleryartimg && (
-        <ImgContainer galleryartimg={galleryartimg}></ImgContainer>
-      )}
-    </>
-  );
-}
-
 const ArtGrid = () => {
   const ContainerTattoo = styled.div`
-    display: grid;
-    gap: 1rem;
-    margin: ${isMobile ? " 0 1rem;" : " 0 10rem;"}
-      ${isMobile
-        ? "grid-template-columns: 1fr 1fr;"
-        : "grid-template-columns: 1fr 1fr 1fr 1fr ;"};
+    margin: ${isMobile ? "" : "0 10rem"};
+  `;
+
+  const ImageGrid = styled(ImageList)`
+    border-radius: 0.2rem;
+    transistion: 1s;
+  `;
+
+  const ImageSelector = styled(ImageListItem)`
+    border-radius: 0.2rem;
+  `;
+
+  const ImgContainer = styled.img`
+    transition: 400ms;
+    filter: sepia(0.9);
+    width: 100%;
+    &:hover {
+      cursor: pointer;
+      filter: none;
+      border: 2px solid ${(props) => props.theme.colorLines};
+    }
   `;
 
   return (
-    <>
-      <ContainerTattoo>
-        {GalleryArray.map((e, GalleryImg) => (
-          <Item key={GalleryImg} galleryartimg={e.GalleryImg} />
-        ))}
-      </ContainerTattoo>
-    </>
+    <ContainerTattoo>
+      <ImageGrid variant="masonry" cols={4} gap={10}>
+        {GalleryArray.slice(1)
+          .sort(() => Math.random() - 0.5)
+          .map((GalleryArray) => (
+            <ImageSelector key={GalleryArray.GalleryImg}>
+              <ImgContainer
+                src={`${GalleryArray.GalleryImg}?w=248&fit=crop&auto=format`}
+                srcSet={`${GalleryArray.GalleryImg}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                alt={GalleryArray.GalleryImg}
+                loading="lazy"
+              />
+            </ImageSelector>
+          ))}
+      </ImageGrid>
+    </ContainerTattoo>
   );
 };
 
